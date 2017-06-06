8c352f807fef587a2ec5cac7fb734c72c758112d
Jürgen Knauth <pubsrc@binary-overflow.de>
2017-06-06 22:51:31 +0200

    Fixing publishing date

--- src/setup.py
+++ src/setup.py
@@ -7,13 +7,13 @@ def readme():
 
 
 setup(name='jk_temporary',
-	version='0.2017.1.13',
+	version='0.2017.6.6',
 	description='Collection of utility functions and classes to manage temporary data.',
 	author='Jürgen Knauth',
 	author_email='pubsrc@binary-overflow.de',
 	license='Apache 2.0',
 	url='https://github.com/jkpubsrc/python-module-jk-temporary',
-	download_url='https://github.com/jkpubsrc/python-module-jk-temporary/tarball/0.2017.1.13',
+	download_url='https://github.com/jkpubsrc/python-module-jk-temporary/tarball/0.2017.6.6',
 	keywords=['temp', 'temporary', 'tempfiles'],
 	packages=['jk_temporary'],
 	install_requires=[
